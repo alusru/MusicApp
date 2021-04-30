719adfa24ea48dbc99b2aa90eb70cfb16a3d28e7
alusru <collen.nkabinde@gmail.com>
2021-04-30 20:59:49 +0200

    _artist fix

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,13 +4,13 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/404.js")),
-  "component---src-pages-album-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/album.js")),
-  "component---src-pages-artist-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/artist.js")),
-  "component---src-pages-home-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/home.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/page-2.js")),
-  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/mbusonkabinde/belle/MusicApp/src/pages/using-typescript.tsx"))
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\systemdev\\MusicApp\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\404.js")),
+  "component---src-pages-album-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\album.js")),
+  "component---src-pages-artist-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\artist.js")),
+  "component---src-pages-home-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\home.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\systemdev\\MusicApp\\src\\pages\\using-typescript.tsx"))
 }
 
