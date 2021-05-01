317a193d053a11ded75a63b376a97b2260f35829
Mbuso Nkabinde <44375076+alusru@users.noreply.github.com>
2021-05-01 08:02:50 +0200

    cors proxy edit

--- src/components/API.js
+++ src/components/API.js
@@ -1,2 +1,2 @@
-const cors_api_host = 'https://thingproxy.freeboard.io/fetch/'
+//const cors_api_host = 'https://thingproxy.freeboard.io/fetch/'
 export const API = `${cors_api_host}https://api.deezer.com/`
