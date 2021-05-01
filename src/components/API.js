ae9ee63ea7595ca8d1800fb6906fb75c2d106a80
Mbuso Nkabinde <44375076+alusru@users.noreply.github.com>
2021-05-01 08:06:31 +0200

    use for localhost

--- src/components/API.js
+++ src/components/API.js
@@ -1,2 +1,9 @@
-//const cors_api_host = 'https://thingproxy.freeboard.io/fetch/'
+
+export const API = `https://api.deezer.com/`
+
+
+/**
+use for localhost
+const cors_api_host = 'https://thingproxy.freeboard.io/fetch/'
 export const API = `${cors_api_host}https://api.deezer.com/`
+**/
