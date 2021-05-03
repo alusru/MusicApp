72a5be16a733596429cc2b536f402860b937a3f6
Mbuso Nkabinde <44375076+alusru@users.noreply.github.com>
2021-05-03 08:46:41 +0200

    update api

--- src/components/API.js
+++ src/components/API.js
@@ -1,5 +1,5 @@
 export const Config = {
     api: `https://api.deezer.com/`,
-    cors_api_host: `https://cors-anywhere.herokuapp.com/`
+    cors_api_host: `https://cors.bridged.cc/`
 };
 export const API = `${Config.cors_api_host}${Config.api}`;
