15504f040c1600d3098bc042e82542d3c9cfda30
Mbuso Nkabinde <44375076+alusru@users.noreply.github.com>
2024-02-10 10:23:44 +0200

    a

--- src/components/API.js
+++ src/components/API.js
@@ -1,2 +1,2 @@
-const cors_api_host = 'https://cors-anywhere.herokuapp.com/'
-export const API = `${cors_api_host}https://api.deezer.com/`
+const cors_api_host = 'https://cors-anywhere.herokuapp.com/';
+export const API = `${cors_api_host}https://api.deezer.com/`;
