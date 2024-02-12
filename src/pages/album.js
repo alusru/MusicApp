96d60b8561fd0ad9ca26b4d2d19230fea813e28c
Mbuso Nkabinde <44375076+alusru@users.noreply.github.com>
2024-02-12 14:57:15 +0200

    bug fix

--- src/pages/album.js
+++ src/pages/album.js
@@ -47,7 +47,7 @@ const Album = (props) => {
 
   const release_date = artist.release_date;
 
-  let days = today.diff(release_date, "days");
+  let days = today?.diff(release_date, "days");
 
   // diff between release date and todays date
   // if diff is greater or equal to 7 than display new
